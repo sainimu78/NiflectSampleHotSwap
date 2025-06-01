b5c48579b47eeb4dc3124dc36b22b43a75233790
sainimu78 <393584307@qq.com>
2025-06-01 19:03:04 +0800

    操作提示

--- Source/App/src/Main.cpp
+++ Source/App/src/Main.cpp
@@ -1,17 +1,28 @@
 #include "Game.h"
 #include <iostream>
 
+#define EXIT_KEY 'q'
+
 int main()
 {
 	auto game = CreateGame();
 	auto actor = Niflect::MakeShared<CActor>();
 	game->AddActor(actor);
 
+	printf(
+R"(--------------------------------------
+1. Hot-Swap:	Press [Enter]
+2. Exit:	Press [%c] then [Enter]
+--------------------------------------
+)", EXIT_KEY);
+
 	game->Start();
+	printf("Started ...\n");
+
 	while (true)
 	{
 		auto key = std::cin.get();
-		if (key == 'q')
+		if (key == EXIT_KEY)
 			break;
 	}
 	game->Stop();
