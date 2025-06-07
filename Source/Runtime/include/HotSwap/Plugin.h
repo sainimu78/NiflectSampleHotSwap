a10aa1c19ae8771b7e28398e0a954ce3939d7169
sainimu78 <393584307@qq.com>
2025-06-08 03:30:49 +0800

    修复 Linux 下 Plugin 来源目录错误

--- Source/Runtime/include/HotSwap/Plugin.h
+++ Source/Runtime/include/HotSwap/Plugin.h
@@ -31,8 +31,10 @@
 
 #ifdef WIN32
 #define CPF_PLATFORM_OS_NAME "Windows"
+#define CPF_FINAL_OUTPUT_DIR_NAME "bin"
 #else
 #define CPF_PLATFORM_OS_NAME "Linux"
+#define CPF_FINAL_OUTPUT_DIR_NAME "lib"
 #endif
 #ifdef NODEBUG
 #define CPF_CONFIG_NAME "Release"
@@ -40,7 +42,7 @@
 #define CPF_CONFIG_NAME "Debug"
 #endif
 
-#define DEFAULT_PLUGIN_DIR_PATH "../../../../../Plugin/" CPF_PLATFORM_OS_NAME "/DefaultBuild/" CPF_CONFIG_NAME "/bin"
+#define DEFAULT_PLUGIN_DIR_PATH "../../../../../Plugin/" CPF_PLATFORM_OS_NAME "/DefaultBuild/" CPF_CONFIG_NAME "/" CPF_FINAL_OUTPUT_DIR_NAME
 
 class CPluginInstance
 {
